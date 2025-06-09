e444b850cf09c10925ae7659e372e7ad363c7ceb
Darek Rossman <darekrossman@me.com>
2025-06-09 11:08:37 -0400

    ok

---
+++ seed.ts
@@ -0,0 +1,62 @@
+/**
+ * ! Executing this script will delete all data in your database and seed it with 10 users.
+ * ! Make sure to adjust the script to your needs.
+ * Use any TypeScript runner to run this script, for example: `npx tsx seed.ts`
+ * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
+ */
+import { createSeedClient } from '@snaplet/seed'
+import bcrypt from 'bcryptjs'
+
+const main = async () => {
+  const seed = await createSeedClient({ dryRun: true })
+
+  // Truncate all tables in the database
+  await seed.$resetDatabase()
+
+  await seed.users([
+    {
+      instance_id: '00000000-0000-0000-0000-000000000000',
+      aud: 'authenticated',
+      role: 'authenticated',
+      email: 'darek@subpopular.dev',
+      encrypted_password: await bcrypt.hash('test123', 10),
+      raw_user_meta_data: {
+        full_name: 'Darek Rossman',
+      },
+      raw_app_meta_data: { provider: 'email', providers: ['email'] },
+      banned_until: null,
+      last_sign_in_at: new Date(),
+      created_at: new Date(),
+      updated_at: new Date(),
+      confirmation_token: '',
+      confirmation_sent_at: null,
+
+      profiles: [
+        {
+          projects: [
+            {
+              brands: [
+                {
+                  product_catalogs: [
+                    {
+                      categories: (x) =>
+                        x(4, {
+                          products: (x) => x(10),
+                        }),
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ])
+
+  console.log('Database seeded successfully!')
+
+  process.exit()
+}
+
+main()
